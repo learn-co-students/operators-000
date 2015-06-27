0e1bf1bb7b70bf60ba35445ece008aa0be4eeb48
Artem Eremin <artross@yandex.ru>
2015-06-27 12:06:22 +0300

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-
+  !(40..60).cover?(speed)
 end
 
 
 
 def not_safe?(speed)
-	
+  #speed < 40 ? true : speed > 60 #they want it differently
+  speed < 40 || speed > 60 ? true : false
 end
 	
 
